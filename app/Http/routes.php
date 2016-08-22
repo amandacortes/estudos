0318f50434cb67bba99fdfba1ceb89fe288426fa
Hugo Henrique Fumero de Souza <hugohfsouza@gmail.com>
2016-08-22 11:18:15 -0300

    iniciando o repositorio

---
+++ app/Http/routes.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Aqui fica registrado todas as todas da aplicação
+|
+*/
+
+Route::get('/', 'WelcomeController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+
+
+Route::get('/404', function(){
+	return view('paginas.erro404'); //retorna a pagina que está em resources/paginas/views/about.blade.php
+});
+
+
+Route::get('/404', function(){
+	$pessoas = ['Hugo', 'Joao', 'juca'];
+	return view('paginas.dadosRota', ['pessoas'=>$pessoas] ); //retorna a pagina que está em resources/paginas/views/about.blade.php
+});
+
+Route::get('/alunos','AlunoController@index'); //vai estar em app/http/controllers
