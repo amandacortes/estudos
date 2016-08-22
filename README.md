6ba4035bce46d8a5f82bb7555c5d26e7a0de1814
Hugo Henrique Fumero de Souza <hugohfsouza@gmail.com>
2016-08-22 11:35:44 -0300

    estudando layouts

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ Thank you for considering contributing to the Laravel framework! The contributio
 ### License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
+
+
+
+php artisan make:controller <nomeDoController>
--- resources/views/paginas/dadosRota.blade.php
+++ resources/views/paginas/dadosRota.blade.php
@@ -1,43 +1,10 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@extends('layout')
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+@section('header')
+<title>Laragvel</title>
+@stop('header')
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
+@section('content')
 		<div class="container">
 			<div class="content">
 				<?php foreach ($pessoas as $pessoa): ?>
@@ -45,5 +12,5 @@
 				<?php endforeach ?>
 			</div>
 		</div>
-	</body>
-</html>
+
+@stop('content')
