f60244f0d3c4fe95bf5fe12d3b33c08d58669827
Amanda C R <amandar.cortes@gmail.com>
2017-10-02 15:26:00 -0300

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,9 +5,10 @@ class ExampleTest extends TestCase {
 	/**
 	 * A basic functional test example.
 	 *
+	 *
 	 * @return void
 	 */
-	public function testBasicExample()
+	public function testBasicExample() 
 	{
 		$response = $this->call('GET', '/');
 
